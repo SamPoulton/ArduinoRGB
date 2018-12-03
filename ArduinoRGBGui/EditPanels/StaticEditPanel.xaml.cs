fabc99e12d84d7a4d139430f2d510c4d71d3fa8e
Sam <Sam@SAMSPC>
2018-12-03 19:26:02 +0000

    oops

--- ArduinoRGBGui/EditPanels/StaticEditPanel.xaml.cs
+++ ArduinoRGBGui/EditPanels/StaticEditPanel.xaml.cs
@@ -18,9 +18,9 @@ namespace ArduinoRGBGui
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class StaticEditPanel : UserControl
     {
-        public UserControl1()
+        public StaticEditPanel()
         {
             InitializeComponent();
         }
